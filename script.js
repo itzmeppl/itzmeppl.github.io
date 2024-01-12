9474b5657d4183cfdb569540bba2d722db1f6724
Riddhima <riddhimagarg03@gmail.com>
2024-01-12 18:37:40 -0500

    attempting retheme

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ document.getElementById('menu-icon').addEventListener('click', function() {
 
 
 // dark mode and light mode
-document.getElementById('retheme').addEventListener('click', function() {
+document.getElementById('re-theme').addEventListener('click', function() {
     document.body.classList.toggle('dark-mode');
 });
