4e83738da54da95f740ede328e3a2f6bff58d517
Riddhima <riddhimagarg03@gmail.com>
2024-01-12 18:31:13 -0500

    dark mode added

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+//responsive design element
+//changes the top bar to become a drop down menu
 document.getElementById('menu-icon').addEventListener('click', function() {
     var topBar = document.getElementById('top-bar');
     topBar.classList.toggle('show-menu');
+});
+
+
+// dark mode and light mode
+var darkModeEnabled = false;
+document.getElementById('dark-mode').addEventListener('click', function() {
+    darkModeEnabled = !darkModeEnabled;
+    document.body.classList.toggle('dark-mode', darkModeEnabled);
 });
