8ab88f94617de412b85a4c994f50f851fc1c3351
Riddhima <riddhimagarg03@gmail.com>
2024-01-12 18:35:30 -0500

    dark mode enabling

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ document.getElementById('menu-icon').addEventListener('click', function() {
 
 
 // dark mode and light mode
-var darkModeEnabled = false;
-document.getElementById('dark-mode').addEventListener('click', function() {
-    
-    document.body.classList.toggle('dark-mode', darkModeEnabled);
+document.getElementById('retheme').addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
 });
